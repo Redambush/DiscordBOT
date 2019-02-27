b092503617bf8c3af65364a7ee997ff63848fa83
Redambush <45522887+Redambush@users.noreply.github.com>
2019-02-27 13:18:31 +0100

    Add files via upload

---
+++ TrustFactor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace TrustFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+        
+    }
+}
